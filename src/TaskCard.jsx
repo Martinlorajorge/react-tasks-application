aaaf1dc6ae08bb007ebaef5a64413a6b5f90a70e
Martin Lora <martinlorajorge@gmail.com>
2023-08-12 23:19:40 -0300

    first commit

---
+++ src/TaskCard.jsx
@@ -0,0 +1,10 @@
+function TaskCard({ task }) {
+	return (
+		<div>
+			<h1>{task.title}</h1>
+			<p>{task.description}</p>
+		</div>
+	);
+}
+
+export default TaskCard;
---
+++ src/task.js
@@ -0,0 +1,17 @@
+export const tasks = [
+	{
+		id: 0,
+		tittle: 'Mi primer tarea',
+		description: 'Mi primer tarea',
+	},
+	{
+		id: 1,
+		tittle: 'Mi segunda tarea',
+		description: 'Mi segunda tarea',
+	},
+	{
+		id: 2,
+		tittle: 'Mi tercer tarea',
+		description: 'Mi tercer tarea',
+	},
+];
