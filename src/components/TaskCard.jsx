3956f0249a5626bbde4ca416d9f0e0a19af37772
Martin Lora <martinlorajorge@gmail.com>
2023-08-16 08:44:45 -0300

    Últimos Cambios

--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -7,7 +7,7 @@ function TaskCard ({ task }) {
 
   return (
     <div className='bg-gray-800 text-white p-4 rounded-md'>
-      <h1 className='text-xl font-bold capitalize'>{task.tittle}</h1>
+      <h1 className='text-xl font-bold capitalize'>{task.title}</h1>
       <p className='text-gray-500'>{task.description}</p>
       <button className='bg-red-500 px-2 py-1 mt-4 rounded-md hover:bg-red-400' onClick={() => deleteTask(task.id)}>Eliminar Tarea</button>
     </div>
--- src/data/task.js
+++ src/data/task.js
@@ -1,17 +1,17 @@
 export const tasks = [
   {
     id: 0,
-    tittle: 'Mi primer tarea',
+    title: 'Mi primer tarea',
     description: 'Mi primer tarea'
   },
   {
     id: 1,
-    tittle: 'Mi segunda tarea',
+    title: 'Mi segunda tarea',
     description: 'Mi segunda tarea'
   },
   {
     id: 2,
-    tittle: 'Mi tercer tarea',
+    title: 'Mi tercer tarea',
     description: 'Mi tercer tarea'
   }
 ]
