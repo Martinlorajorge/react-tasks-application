af30e1b85927ccf9f9479ac0e90b77403269bd53
Martin Lora <martinlorajorge@gmail.com>
2023-08-16 08:34:14 -0300

    link-repo

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://github.com/Martinlorajorge/react-tasks-application/'
+  base: 'https://martinlorajorge.github.io/react-tasks-application/'
 })
